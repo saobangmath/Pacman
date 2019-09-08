55ef39009854fe7b2dc4439aabb4a5291e82f7b4
AFatWolf <ducthanhdang2512@gmail.com>
2019-09-09 00:25:37 +0900

    Pacman added

---
+++ Animation.cpp
@@ -0,0 +1,20 @@
+#ifndef CONSTANT
+    #include "Constant.h"
+    #define CONSTANT
+#endif // CONSTANT
+#include "SFML/Graphics.hpp"
+#include "Animation.h"
+
+Frame::Frame(sf::IntRect rect): rect(rect) {
+}
+
+AnimationSpritesSheet::AnimationSpritesSheet(sf::Sprite& target): target(target) {
+}
+
+void AnimationSpritesSheet::addFrame(Frame frame) {
+    frames.push_back(frame);
+}
+
+sf::Sprite& AnimationSpritesSheet::getCurrentFrame() {
+    return target;
+}
